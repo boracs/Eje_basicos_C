9673cd596966d99b58f64ee7cd22ac97b4d6349d
boracs <msndeborjabolado@gmail.com>
2024-11-04 09:52:38 +0100

    ordenacion binaria

--- Arrays/C2_arrays_33/C2_arrays_33/C2_arrays_33.c
+++ Arrays/C2_arrays_33/C2_arrays_33/C2_arrays_33.c
@@ -14,21 +14,15 @@ main() {
 	SetConsoleOutputCP(1252);
 
 	int arr[POSICIONES];
-
 	/*
 	33)Escribir un programa para cargar una tabla con 20 números enteros y visualice el
 	resultado de la siguiente operación: n1 - n2 + n3 - n4 + ........................ + n19 - n20.
 */
-
 	for (int i = 0; i < POSICIONES; i++) {
 		printf("dame el %dº numero;", i + 1);
 		scanf_s("%d", &arr[i]);
 	}
-	 
-
-
 	int suma = 0;
-
 	for (int i = 0; i < POSICIONES; i++) {
 		if (i % 2 ==0 ) {
 			suma = suma + arr[i];
@@ -37,7 +31,6 @@ main() {
 			suma = suma - arr[i];
 		}
 	}
-
 	printf("total el numero final es: %d", suma);
 
 
--- Arrays/C2_arrays_34/C2_arrays_34/C2_arrays_34.c
+++ Arrays/C2_arrays_34/C2_arrays_34/C2_arrays_34.c
@@ -5,7 +5,7 @@
 
 #include<stdio.h>
 #include<Windows.h>
-#define pos 9
+#define pos 5
 main() {
 
 	SetConsoleCP(1252);
@@ -17,15 +17,42 @@ main() {
 	34)Escribir un programa que pida números entre 1 y 9 y cuando tecleen 0, visualizar un
 	informe donde se indica cada uno de los números y las veces que lo han tecleado.
 */
-	
-
 	int arrDeNum[pos];
+	// int arrDeNum[pos] = {0,0,0,0,0,0,0,0,0};    // Aqui inicialicoy declaro a la vez pero,  porque no puedo declarar la variable e nicializarla por separado. es dcir:
+				// int arrDeNum[pos];
+				// arrDeNum = {0,0,0,0,0,0,0,0,0};
 
+	//inicializo todos los valores del array a 0;
+	for (int i = 1; i < pos; i++) {
+		arrDeNum[i] = 0;
+	}
 
+	// pido mel numeor una  primera vez
+	int num ; 
+	printf("Introduzca un numero del 1 al %d  que quiera para sumar mas uno en esa posicion, cero si quiere finalizar\n", pos);
+	scanf_s("%d", &num);
+	//compruebo que el numer introducido esta entre lso valores requeridos sino vuelvoa  pedir
+	while (num < 0 || num > pos)
+	{
+		printf("___ERROR____El numeor no es vlaido met eun numoer del  1 al %d  porfavor o  cero si quiere finalizar \n", pos);
+		scanf_s("%d", &num);
+	}
 
-
-
-
+	//mientras el numero introducido sea distinto de cero vyuelve a pedir todo:
+	while (num != 0) {
+		arrDeNum[num - 1]++;  //incremento en mas uno el valor de la posicion escogida
+		printf("Introduzca un numero del 1 al %d  que quiera para sumar mas uno en esa posicion, cero si quiere finalizar\n", pos);
+		scanf_s("%d", &num);
+		//si el numeor introducido no esta entr elos valores requeridos vuelvo a preguntar todo
+		while (num < 0 || num > pos)
+		{
+			printf("___ERROR____El numeor no es vlaido met eun numoer del  1 al %d  porfavor o  cero si quiere finalizar \n", pos);
+			scanf_s("%d", &num);
+		}
+	}
+	for (int i = 0; i < pos; i++) {
+		printf("eL NUMERO %d la metiste: %d veces \n", i+1, arrDeNum[i]);
+	}
 
 }
 
