85fc720ffefb4c064021f5af50ea36167d8079ee
boracs <msndeborjabolado@gmail.com>
2024-11-18 08:45:39 +0100

    ordenacion por lso dos metodos de todas las maneras completado

--- Arrays/Ordenacion_por_burbuja/Ordenacion_por_metodo_burbuja/Ordenacion_por_metodo_burbuja/Ordenacion_por_metodo_burbuja.c
+++ Arrays/Ordenacion_por_burbuja/Ordenacion_por_metodo_burbuja/Ordenacion_por_metodo_burbuja/Ordenacion_por_metodo_burbuja.c
@@ -1,8 +1,4 @@
 
-
-
-
-
 #include<stdio.h>
 #include<Windows.h>
 #define POS 10
@@ -12,19 +8,26 @@ main() {
 	SetConsoleCP(1252);
 	SetConsoleOutputCP(1252);
 
+	//creo el array
 	int arr[POS] = { 110, 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 };
 
+/*
+	INDICE:
+	1- BURBUJA ASC mayor al final ( para hacer descendente simplemente cambio e simbolo en el condicional de comparacion `<`)
+	2- BURBUJA ASC menor al principio ( para hacer descendente simplemente cambio e simbolo en el condicional de comparacion `<`)
+	3- SELECCION ASC el primeor con el resto ( para hacer descendente simplemente cambio e simbolo en el condicional de comparacion `<`)
+	4- SEÑECCION ASC el ultimo con le resto ( para hacer descendente simplemente cambio e simbolo en el condicional de comparacion `<`)
 
+ */
+	
 
+//----------------------------------------------------BURBUJA----ASC-------------------------------------------------------
 
-
-
-
-	// 1º-  metodo ordenacion burbuja llevando el mayor al final
+	// 1º-  metodo ordenacion BURBUJA ascendente  llevando el mayor al final
 	// { 110, 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 };
 	// { 99, 3, 1, 101, 2, 5, 8, 15, 21, 110 };
 	// { 99, 3, 1, 2, 5, 8, 15, 21,101, 110 };
-   //  { 3, 1, 2, 5, 8, 15, 21, 99, 101, 110 };
+    //  { 3, 1, 2, 5, 8, 15, 21, 99, 101, 110 };
 	/*
 	for (int i = 0; i < POS-1; i++) {
 
@@ -39,13 +42,9 @@ main() {
 		}
 	}
 	*/
+//----------------------------------------------------BURBUJA----ASC-------------------------------------------------------------
 
-
-
-
-
-
-	// 2º- metodo ordenacion burbuja llevando el menor al principio
+	// 2º- metodo ordenacion burbuja ascendente  llevando el menor al principio
 	// { 110, 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 };
 	// { 1, 110, 99, 3, 101 ,2 ,5 ,8 ,15 ,21 };
 	// { 1, 2, 110, 99, 3, 101 ,5 ,8 ,15 ,21 };
@@ -62,10 +61,13 @@ main() {
 	}
 	*/
 
+//-------------------------------------------------SELECCION------ASC----------------------------------------------------------------
 
+	// 3º metodo ordenacion seleccion ascendente comparando el primero que sea menor que e resto y si no lo es se cambia.
 
-	// metodo ordenacion seleccion comparando el primero que sea menor que e resto y si no lo es se cambia.
-	
+	// { 110, 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 };
+	// { 1, 110 , 99, 3, 101 ,2 ,5 ,8 ,15 ,21 };
+	// { 1, 2 110, 99, 3 , 101  ,5 ,8 ,15 ,21 };
 	/*
 		for (int i = 0; i < POS-1; i++) {
 			int posMin = i;
@@ -80,33 +82,32 @@ main() {
 		}
 	*/
 
-		// metodo ordenacion seleccion comparando el ultimo  que sea mayor que e resto y si no lo es se cambia.
-
+//--------------------------------------------------SELECCION-------ASC---------------------------------------------------------------
+	
+	//4º  metodo ordenacion seleccion ascendente  comparando el ultimo  que sea mayor que el resto y si no lo es se cambia.
+	
+	// { 110, 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 };
+	// { 99, 3, 1, 101 ,2 ,5 ,8 ,15 ,21 , 110};
+	// { 3, 1, 101 ,2 ,5 ,8 ,15 , 21 ,99 , 110};
+	/*
 		for (int i = 0; i < POS-1; i++) {
-			int posMax = i;
-			for (int j = i; j < POS; j++) {
+			int posMax = POS-i-1;
+			for (int j =0; j < POS-i; j++) {
 				if (arr[posMax] < arr[j]) {  
 					posMax = j;
 				}
 			}
 			int aux = arr[posMax];
-			arr[posMax] = arr[i];
-			arr[i] = aux;
+			arr[posMax] = arr[POS-i-1];
+			arr[POS-i-1] = aux;
 		}
+	*/
 
-
-
-
-
-
+//------------------------------------------------------------------------------------------------------------------------------------
 
 	// imprimo array
 	for (int i = 0; i < POS; i++) {
 		printf("La posicion % dº es el %d\n", i, arr[i]);
 	}
-
-
-
-
 }
 
