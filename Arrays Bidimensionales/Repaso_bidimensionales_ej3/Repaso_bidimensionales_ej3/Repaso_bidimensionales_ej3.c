5bed5fdb40549a7a3c10c1f3216ab389a24aa2c7
boracs <msndeborjabolado@gmail.com>
2024-11-29 09:46:28 +0100

    ej 3 en trabajo

--- Arrays Bidimensionales/Repaso_bidimensionales_ej3/Repaso_bidimensionales_ej3/Repaso_bidimensionales_ej3.c
+++ Arrays Bidimensionales/Repaso_bidimensionales_ej3/Repaso_bidimensionales_ej3/Repaso_bidimensionales_ej3.c
@@ -11,34 +11,126 @@
 
 //FUNCIONES--------------------------------------------
 
+//PEDIR FILA
 int pedir_fila() {
 	int fila = 1;
 	do {
 		printf("Que fila deseas reserbar?");
 		scanf_s("%d", &fila);
+		if (fila < 1 || fila>10) {
+			printf("______ERROR_____Dame una FILA valida");
+		}
 	} while (fila < 1 || fila>10);
 	fila = fila - 1;
 	return fila;
 }
 
+//PEDIR BUTACA
 int pedir_butaca() {
 	int butaca = 1;
 	do {
 		printf("Que butaca deseas reserbar?");
 		scanf_s("%d", &butaca);
+		if (butaca < 1 || butaca>15) {
+			printf("______ERROR_____Dame una BUTACA valida");
+		}
 	} while (butaca < 1 || butaca>15);
 	butaca = butaca - 1;
+	return butaca;
 }
 
-int reservar(int arr[FIL][BUT]) {
+//REALIZAR RESERVA
+int realizarReserva(int arr[FIL][BUT]) {
 
+	int procesoReservaFinaslizado = 0;
+	int respuestaCorrecta = 0;
+	do {
+
+		int fila = pedir_fila();
+		int butaca = pedir_butaca();
+
+		if (arr[fila][butaca] == 0) {
+					printf("Reserva realizada EXITOSAMENTE");
+					arr[fila][butaca] == 1;
+					procesoReservaFinaslizado = 1;
+					}
+		else {
+			printf("IMPOSIBLE REALZIAR RESERVA, la butaca ya esta adjudicada");
+			int respuestaCorrecta = 0;
+			char repetir = ' ';
+			do {
+				printf("Desea volver a intentar realizar la reserva 'S' o'N' ?");
+				scanf_s("%c", &repetir, 1);
+				if (repetir == 'S' || repetir == 'N' || repetir == 's' || repetir == 'n') {
+					respuestaCorrecta = 1;
+				}
+			} while (respuestaCorrecta == 0);
+			if (repetir == 'N' || repetir == 'n') {
+				procesoReservaFinaslizado = 1;
+			}
+		}
+	} while (procesoReservaFinaslizado == 0);
 
 }
 
+//REALIZAR CANCELACION
+
+int realizarCancelacion(int arr[FIL][BUT]) {
+
+	int procesoCancelacionFinaslizado = 0;
+	int respuestaCorrecta = 0;
+	do {
+
+		int fila = pedir_fila();
+		int butaca = pedir_butaca();
+
+		for (int i = 0; i < FIL; i++) {
+			if (i == fila) {
+				for (int j = 0; j < BUT; j++) {
+					if (j == butaca) {
+						if (arr[i][j] == 1) {
+							printf("Cancelacion realizada EXITOSAMENTE");
+							arr[i][j] == 0;
+							procesoCancelacionFinaslizado == 1;
+						}
+						else {
+							printf("IMPOSIBLE  CANCELAR la reserva, LA BUTACA YA ESTABA LIBRE");
+							int respuestaCorrecta = 0;
+							char repetir = ' ';
+							do {
+								printf("Desea volver a intentar realizar la cancelacion 'SI' o'NO' ?");
+								scanf_s("%c", &repetir, 2);
+								if (repetir == 'SI' || repetir == 'NO' || repetir == 'si' || repetir == 'no') {
+									respuestaCorrecta = 1;
+								}
+							} while (respuestaCorrecta == 0);
+							if (repetir == 'NO' || repetir == 'no')
+								procesoCancelacionFinaslizado == 1;
+						}
+					}
+				}
+			}
+		}
+	} while (procesoCancelacionFinaslizado == 0);
+
+}
+
+//MOSTRAR INFORME
+void  mostrarInforme(int arr[FIL][BUT]) {
 
 
+	for (int i =0; i < FIL; i++) {
+		for (int j = 0; j < BUT; j++) {
+			printf("_%d_", arr[i][j]);
+		}
+		printf("\n");
+	}
 
+}
 //fin funciones----------------------------------------
+
+
+
 main() {
 
 	SetConsoleCP(1252);
@@ -64,42 +156,35 @@ e) Salir.
 */
 	//declaro arrays;
 	int plazas[FIL][BUT];
-
 	//bucle principal del menu que se repite hasta que se meta opcion == 4
 	int opcion = 1;
 	do {
 		//bucle para asegurar que la opcion esta entre el 1 y el 4:
 		do {
-			printf("Dime que desea realizar: \n 1)reservar \n 2) devolver \n 3)mostrar informe \n 4)salir");
+			printf("Dime que desea realizar: \n 0)RESETEAR ARRAY \n 1)reservar \n 2) devolver \n 3)mostrar informe \n 4)salir");
 			scanf_s("%d", &opcion);
-		} while (opcion < 1 || opcion>4);
-
+		} while (opcion < 0 || opcion>4);
 		//condicionales del menu
-		if (opcion == 1) {
 
-			printf("Opcion 1 ESCOGIDA:\n");
-			//pido fila y butaca (esto iria en una funcion)
-			int fila = 1;
-			do {
-				printf("Que fila deseas reserbar?");
-				scanf_s("%d", &fila);
-			} while (fila < 1 || fila>10);
-			int butaca = 1;
-			do {
-				printf("Que butaca deseas reserbar?");
-				scanf_s("%d", &butaca);
-			} while (butaca < 1 || butaca>15);
-			fila = fila - 1;
-			butaca = butaca - 1;
-
-
-
+		if (opcion == 0) {
+			printf("Opcion 0 ESCOGIDA reseteando plazas....:\n"); //inicializo todas las plazas a 0 (estan libres); RESETEA LA SALA
+			for (int i = 0; i < FIL; i++) {
+				for (int j = 0; j < BUT; j++) {
+					plazas[i][j] = 0;
+				}
+			}
+		}
+		else if (opcion == 1) { //reservo
+			printf("Opcion 1  ESCOGIDA reservando....:\n");
+			realizarReserva(plazas);
 		}
 		else if (opcion == 2) {
-			printf("Opcion 2  ESCOGIDA:\n");
+			printf("Opcion 2  ESCOGIDA cencelando....:\n");
+			realizarCancelacion(plazas);
 		}
 		else if (opcion == 3) {
-			printf("Opcion 3  ESCOGIDA:\n");
+			printf("Opcion 3  ESCOGIDA mostrando informe.....:\n");
+			mostrarInforme(plazas);
 		}
 		else{
 			printf("De acuerdo salgamos:Adios\n");
